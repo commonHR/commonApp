36d7653e347dd84124ba10d0c6c59c46713daad7
Aaron Rice <adrice727@gmail.com>
2014-06-09 09:10:53 -0700

    Includes Android production build version 0.0.4

--- platforms/android/ant-build/Common-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/Aaron/tweetUpApp/platforms/android/ant-build/Common-debug-unaligned.apk : \
-/Users/Aaron/tweetUpApp/platforms/android/ant-build/Common.ap_ \
-/Users/Aaron/tweetUpApp/platforms/android/ant-build/classes.dex \
-/Users/Aaron/tweetUpApp/platforms/android/CordovaLib/ant-build/classes.jar \
